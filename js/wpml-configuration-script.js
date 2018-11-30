ae06f6d6c1daf28a1bced7d56c469b5710b3bb8d
Kyle Rumble <kylerumble@gmail.com>
2018-11-30 15:36:42 -0800

    Tweaked instructions

--- js/wpml-configuration-script.js
+++ js/wpml-configuration-script.js
@@ -6,7 +6,7 @@
         .after( '<div class="wpml-section wpml-section-notice"><div class="updated below-h2"><h2>Following these instructions to generate the WPML XML:</h2><p>1. Go to <a href="#ml-content-setup-sec-cf">Custom Fields Translation</a> and click "Show system fields"</p><p>2. Go to <a href="#ml-content-setup-sec-cf">Custom Fields Translation</a> and click "Display all results"</p><p>3. Right click mouse and choose "Inspect" and then view console</p><p>4. Click the <a href="#ml-content-setup-sec-generate-xml">Generate XML Button</a></p><p>5. Click the copy button inside the "Console" to grab the XML</p></div></div>' ); 
     
     $( 'body.wpml_page_wpml-translation-management-menu-settings #ml-content-setup-sec-tcf' )
-    .before( '<div class="wpml-section wpml-section-tcf-translation" id="ml-content-setup-sec-generate-xml"><div class="wpml-section-header"><h3>Generate XML</h3><p>Make sure system fields are set to "show" and that "Custom Fields" are set to "display all results"</p></div><div class="wpml-section-content wpml-section-content-wide"><p><button id="generate-xml-button" class="button-primary">Generate XML</button></p></div></div>' );  
+    .before( '<div class="wpml-section wpml-section-tcf-translation" id="ml-content-setup-sec-generate-xml"><div class="wpml-section-header"><h3>Generate XML</h3><p>Make sure system fields are set to "show" and that "Custom Fields" are set to "Display all results"</p></div><div class="wpml-section-content wpml-section-content-wide"><p><button id="generate-xml-button" class="button-primary">Generate XML</button></p></div></div>' );  
     $( "#generate-xml-button" ).on( "click", function(event) {
         _s_execute_xml();
         event.preventDefault();
