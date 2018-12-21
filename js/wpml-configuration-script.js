a344780582b011c0de259b548b5e1d42584610bb
Kyle Rumble <kylerumble@gmail.com>
2018-12-20 18:19:44 -0800

    minor reformatting

--- js/wpml-configuration-script.js
+++ js/wpml-configuration-script.js
@@ -12,16 +12,16 @@
         .after( '<div class="wpml-section wpml-section-notice"><div class="updated below-h2"><h2>Following these instructions to generate the WPML XML:</h2><p>1. Go to <a href="#ml-content-setup-sec-cf">Custom Fields Translation</a> and click "Show system fields"</p><p>2. Go to <a href="#ml-content-setup-sec-cf">Custom Fields Translation</a> and click "Display all results"</p><p>3. Right click mouse and choose "Inspect" and then view console</p><p>4. Click the <a href="#ml-content-setup-sec-generate-xml">Generate XML Button</a></p><p>5. Click the copy button inside the "Console" to grab the XML</p></div></div>' ); 
     
     $mcs_custom_fields_translation_section
-    .before( '<div class="wpml-section wpml-section-tcf-translation" id="ml-content-setup-sec-generate-xml"><div class="wpml-section-header"><h3>Generate XML</h3><p>Make sure system fields are set to "show" and that "Custom Fields" are set to "Display all results"</p></div><div class="wpml-section-content wpml-section-content-wide"><p><button id="mh-generate-xml-button" class="button-primary">Generate XML</button></p><p><textarea name="mh-wpml-xml-config-copy" id="mh-wpml-xml-config-copy" rows="10" style="width: 100%";"></textarea></p></div></div>' );  
+        .before( '<div class="wpml-section wpml-section-tcf-translation" id="ml-content-setup-sec-generate-xml"><div class="wpml-section-header"><h3>Generate XML</h3><p>Make sure system fields are set to "show" and that "Custom Fields" are set to "Display all results"</p></div><div class="wpml-section-content wpml-section-content-wide"><p><button id="mh-generate-xml-button" class="button-primary">Generate XML</button></p><p><textarea name="mh-wpml-xml-config-copy" id="mh-wpml-xml-config-copy" rows="10" style="width: 100%";"></textarea></p></div></div>' );  
     
     $( "#mh-generate-xml-button" ).on( "click", function(event) {                    
         _s_execute_xml();
         event.preventDefault();
     });
 
-  function _s_execute_xml() {
+    function _s_execute_xml() {
       var xmlString = '';
-
+    
       var $rows = $('#icl_cf_translation > .wpml-translation-setup-table > .wpml-flex-table-body .wpml-flex-table-row' );
       
       console.log($rows.html());
@@ -52,6 +52,6 @@
         xmlString += '</custom-fields></wpml-config>';
         $('#mh-wpml-xml-config-copy').val(xmlString);
       }
-  }
+    }
 
 }(document, window, jQuery));
